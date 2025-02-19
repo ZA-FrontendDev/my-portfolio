d2c0f62cdfad02a518e91fbed4250a5a3c0bee97
Zain Ahsan <thdevtwo@gmail.com>
2025-02-19 12:37:37 +0500

    email setup

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
@@ -7,35 +8,39 @@ import Image from "next/image";
 
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
+    const serviceId = "service_kz09dae"; // Replace with your EmailJS Service ID
+    const templateId = "template_whgv3db"; // Replace with your EmailJS Template ID
+    const userId = "Al78hgmGQ5Xo8CAcP"; // Replace with your EmailJS Public Key
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
+    try {
+      await emailjs.send(
+        serviceId,
+        templateId,
+        {
+          from_name: formData.fullname, // Replace with your actual name
+          from_email: formData.email,
+          subject: formData.subject,
+          message: formData.message,
+        },
+        userId
+      );
       setEmailSubmitted(true);
+    } catch (error) {
+      console.error("Email sending failed:", error);
     }
   };
 
@@ -44,13 +49,11 @@ const EmailSection = () => {
       id="contact"
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
           Let&apos;s Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
@@ -76,6 +79,24 @@ const EmailSection = () => {
           <form className="flex flex-col" onSubmit={handleSubmit}>
             <div className="mb-6">
               <label
+                htmlFor="fullname"
+                className="text-white block mb-2 text-sm font-medium"
+              >
+                Your Name
+              </label>
+              <input
+                name="fullname"
+                type="text"
+                id="fullname"
+                required
+                value={formData.fullname}
+                onChange={handleChange}
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="jacob"
+              />
+            </div>
+            <div className="mb-6">
+              <label
                 htmlFor="email"
                 className="text-white block mb-2 text-sm font-medium"
               >
@@ -86,6 +107,8 @@ const EmailSection = () => {
                 type="email"
                 id="email"
                 required
+                value={formData.email}
+                onChange={handleChange}
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="jacob@google.com"
               />
@@ -102,6 +125,8 @@ const EmailSection = () => {
                 type="text"
                 id="subject"
                 required
+                value={formData.subject}
+                onChange={handleChange}
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Just saying hi"
               />
@@ -116,6 +141,9 @@ const EmailSection = () => {
               <textarea
                 name="message"
                 id="message"
+                required
+                value={formData.message}
+                onChange={handleChange}
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Let's talk about..."
               />
