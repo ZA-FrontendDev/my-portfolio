d3a9e42afec5c3f32a4aba5a99f7df03244258a6
Zain Ahsan <mohammadzain048@gmail.com>
2026-06-13 17:36:01 +0500

    cv download

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,8 +49,8 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1aCYwE2L0ihdEvqjS10_Jur6ZAD4tky-U/view?usp=sharing"
-              target="_blank"
+              href="/images/zain_resume.docx"
+              download="Muhammad_Zain_Ahsan_CV.docx"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
